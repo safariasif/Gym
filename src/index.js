ff94c292c36a36fb61f95a86f0d89c125fdde11e
M Asif Safari <91109430+safariasif@users.noreply.github.com>
2022-05-04 10:04:20 +0430

    Revert "Awards"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import {createRoot} from 'react-dom/client'
-import bootstrap from 'bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
 import { App } from './app';
 
 const container = document.getElementById('root');
